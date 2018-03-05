a73bca13f90454467bcfad768e5476fd397450f5
lucas renan <contato@lucasrenan.com>
2018-03-05 02:33:26 +0100

    Refactor fleet calculator

--- lib/fleet/calculator.rb
+++ lib/fleet/calculator.rb
@@ -10,25 +10,33 @@ module Fleet
 
     def fleet_engineers
       total_engineers = 0
-      max_engineers_savings = 0
+      max_engineers_saved = 0
 
-      scooters.each do |s|
-        engineers = 0
-        engineers_with_manager = 0
-        engineers_savings = 0
+      scooters.each do |scooters_number|
+        engineers = possible_engineers_needed(scooters_number)
+        engineers_with_manager = possible_engineers_with_manager(scooters_number)
 
-        engineers = (s / engineer_capacity.to_f).ceil
+        engineers_saved = possible_engineers_saved(engineers, engineers_with_manager)
+        max_engineers_saved = engineers_saved if engineers_saved > max_engineers_saved
+
         total_engineers += engineers
-
-        engineers_with_manager = [((s - manager_capacity) / engineer_capacity.to_f).ceil, 0].max
-
-        engineers_savings = engineers - engineers_with_manager.to_i
-        if (engineers_savings > max_engineers_savings)
-          max_engineers_savings = engineers_savings
-        end
       end
 
-      total_engineers - max_engineers_savings
+      total_engineers - max_engineers_saved
+    end
+
+  private
+    def possible_engineers_needed(scooters_number)
+      (scooters_number / engineer_capacity.to_f).ceil
+    end
+
+    def possible_engineers_with_manager(scooters_number)
+      remaining_scooters = scooters_number - manager_capacity
+      [(remaining_scooters / engineer_capacity.to_f).ceil.to_i, 0].max
+    end
+
+    def possible_engineers_saved(engineers, engineers_with_manager)
+      engineers - engineers_with_manager
     end
   end
 end
--- spec/fleet/calculator_spec.rb
+++ spec/fleet/calculator_spec.rb
@@ -3,17 +3,17 @@ require 'spec_helper'
 RSpec.describe Fleet::Calculator do
   describe '#fleet_engineers' do
     test_data = [
-      {scooters: [15, 10], manager: 12, engineers: 5, expected: 3},
-      {scooters: [11, 15, 13], manager: 9, engineers: 5, expected: 7},
-      {scooters: [11, 15, 13, 5], manager: 9, engineers: 5, expected: 8}
+      {scooters: [15, 10], manager_capacity: 12, engineer_capacity: 5, expected: 3},
+      {scooters: [11, 15, 13], manager_capacity: 9, engineer_capacity: 5, expected: 7},
+      {scooters: [11, 15, 13, 5], manager_capacity: 9, engineer_capacity: 5, expected: 8}
     ]
 
     test_data.each do |data|
-      context "given scooters: #{data[:scooters]}, manager: #{data[:manager]}, engineers: #{data[:engineers]}" do
-        it 'calculates the minimum number of engineers' do
+      context "given scooters: #{data[:scooters]}, manager_capacity: #{data[:manager_capacity]}, engineer_capacity: #{data[:engineer_capacity]}" do
+        it 'calculates the minimum number of engineer_capacity' do
           calculator = described_class.new(data[:scooters],
-                                           data[:manager],
-                                           data[:engineers])
+                                           data[:manager_capacity],
+                                           data[:engineer_capacity])
 
           expect(calculator.fleet_engineers).to eq(data[:expected])
         end
